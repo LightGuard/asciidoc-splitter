54177cc8d3bccd2e600374abcec34b721c7ff157
Jason Porter <lightguard.jp@gmail.com>
2020-02-29 00:36:24 -0700

    Everything but DescriptionLists is in!
    Bunch of tests too!!

--- src/main/java/com/redhat/documentation/asciidoc/SourceExtractor.java
+++ src/main/java/com/redhat/documentation/asciidoc/SourceExtractor.java
@@ -1,9 +1,12 @@
 package com.redhat.documentation.asciidoc;
 
+import java.util.StringJoiner;
+
 import org.asciidoctor.ast.Block;
 import org.asciidoctor.ast.ContentNode;
 import org.asciidoctor.ast.DescriptionList;
 import org.asciidoctor.ast.List;
+import org.asciidoctor.ast.ListItem;
 import org.asciidoctor.ast.Section;
 import org.asciidoctor.ast.StructuralNode;
 import org.asciidoctor.ast.Table;
@@ -12,43 +15,55 @@ public class SourceExtractor {
     private ContentNode node;
     private StringBuilder source;
 
+    enum BlockType {
+        OPEN("--"),
+        QUOTE("____"),
+        VERSE("____"),
+        SIDEBAR("****"),
+        FENCED("```"),
+        LITERAL("...."),
+        PASSTHROUGH("++++"),
+        STEM("++++"),
+        ADMONITION("===="),
+        SOURCE("----"),
+        PARAGRAPH(""),
+        LISTING("----");
+
+        private String delimiter;
+
+        BlockType(String delimiter) {
+            this.delimiter = delimiter;
+        }
+
+        public String delimiter() {
+            return this.delimiter;
+        }
+    }
+
     public SourceExtractor(ContentNode node) {
         this.node = node;
         this.source = new StringBuilder();
 
+        extractMetadata();
+
         if (node instanceof Section)
             extractSection();
 
-        if (node instanceof Block) {
-            switch (node.getContext()) {
-                case "paragraph":
-                    extractParagraph();
-                    break;
-                // TODO: Add more
-            }
-        }
+        if (node instanceof Block)
+            extractBlock();
 
+        if (node instanceof Table)
+            extractTable();
+
+        if (node instanceof List)
+            extractList();
+
+        if (node instanceof DescriptionList)
+            extractDescriptionList();
     }
 
     public String getSource() {
         return source.toString();
-    }
-
-    // TODO: Block types:
-    //        Open: --
-    //        Quote && Verse: ____
-    //        sidebar: ****
-    //        Fenced: ```
-    //        Literal: ....
-    //        Passthrough && Stem: ++++
-    //        Table: |===
-    //        Admonition: ====
-    //        Source && Listing: ----
-
-    private void extractParagraph() {
-        extractMetadata();
-
-        source.append(((Block) node).getSource());
     }
 
     private void extractSection() {
@@ -64,46 +79,115 @@ public class SourceExtractor {
         var title = section.getTitle();
         var level = section.getLevel() < 2 ? 1 : section.getLevel() - 1;
 
-        extractMetadata();
-        source.append("\n").append("=".repeat(Math.max(0, level))).append(" ").append(title).append("\n");
-        source.append(":context: ").append(context);
+        source
+                // Metadata first
+                .append("\n")
+                .append("=".repeat(Math.max(0, level))).append(" ").append(title)
+                .append("\n")
+                .append(":context: ").append(context);
     }
 
-    private void extractListing() {
+    private void extractBlock() {
         var block = (Block) node;
-        // Regex to look for the numbered callout in regular source and xml
 
-        // TODO: add the attributes
-        // TODO: add title
-        extractMetadata();
-
-        var hasCallout = block.getSource().matches("<(!--)?(\\d+|\\.)(--)?>");
-        if ((hasCallout)) {
+        // the metadata comes first and does not end in a new line
+        if (source.length() > 0)
             source.append("\n");
-        } else {
-            source.append("\n\n");
-        }
+
+        // depending on the type of block, there are different delimiters
+        // This should work for all different types of blocks
+        var delimiter = BlockType.valueOf(block.getContext().toUpperCase()).delimiter();
+
+        // This is mostly for paragraphs which have no delimiter
+        // and so they don't need a new line before or after the delimiter
+        var joiner = (delimiter.isEmpty()) ? new StringJoiner("")
+                                           : new StringJoiner("", delimiter + "\n", "\n" + delimiter);
+        source.append(joiner.add(block.getSource()));
     }
 
-    private void extractCalloutList() {
+//        Regex to look for the numbered callout in regular source and xml
+//        var hasCallout = block.getSource().matches("<(!--)?(\\d+|\\.)(--)?>");
+//        if ((hasCallout)) {
+//            source.append("\n");
+//        } else {
+//            source.append("\n\n");
+//        }
+
+    private void extractList() {
         var list = (List) node;
+
+        // It SHOULD be safe to assume that there's at least one entry
+        var tempMarker = ((ListItem) list.getItems().get(0)).getMarker();
+
+        // Do a little dance to get just the marker if they used numbers before the marker
+        var marker = tempMarker.matches("\\d+\\.") ? tempMarker.replaceAll("\\d", "")
+                                                         : tempMarker;
+        var listItemJoiner = new StringJoiner("\n" + marker + " ", marker + " ", "");
+
+        list.getItems().forEach(node -> {
+            // Nested lists, should also work if a list has more than just a basic text for an item
+            var item = (ListItem) node;
+            if (item.getBlocks() != null && item.getBlocks().size() > 0) {
+                // We need the item that has the nested list or block as well
+                StringBuilder itemSource = new StringBuilder(item.getSource());
+
+                // Get all the blocks or nested lists
+                item.getBlocks().forEach(listItemBlock -> {
+                    itemSource.append("\n");
+
+                    // Need to attach the block to the list
+                    // However, nested lists don't need the "+"
+                    if (!((listItemBlock instanceof ListItem) || (listItemBlock instanceof List)))
+                        itemSource.append("+\n");
+
+                    itemSource.append(new SourceExtractor(listItemBlock).getSource());
+                });
+                // Now add list/blocks to the main list
+                listItemJoiner.add(itemSource);
+            } else { // Just a normal list item, nothing special
+                listItemJoiner.add(item.getSource());
+            }
+        });
+
+        // new line after the metadata
+        if (source.length() > 0)
+            source.append("\n");
+
+        if (list.getTitle() != null)
+            source.append(".").append(list.getTitle()).append("\n");
+
+        source.append(listItemJoiner.toString());
     }
 
     private void extractTable() {
         var table = (Table) node;
-    }
+        var delimiter = "|===";
 
-    private void extractList() {
-        var list = (List) node;
+        var bodyJoiner = new StringJoiner("\n| ", "\n| ", "");
+
+        // We need to do a depth first join to make sure it lays out correctly
+        // TODO: what about a nested table?
+        table.getBody().forEach(row -> {
+            var cellJoiner = new StringJoiner(" | ");
+            row.getCells().forEach(cell -> {
+                cellJoiner.add(cell.getSource());
+            });
+            bodyJoiner.merge(cellJoiner);
+        });
+
+        source
+                // New line for after the metadata
+                .append("\n")
+                .append(delimiter)
+                // Now the actual body
+                .append(bodyJoiner)
+                .append("\n")
+                .append(delimiter);
     }
 
     private void extractDescriptionList() {
         var descriptionList = (DescriptionList) node;
     }
-
-    // TODO: context - Listing (need the attributes [style, language, title, subs])
-    //                 A listing is (in this context) a block of code
-    //                 If there are call outs the next block will be a listing block
 
     private boolean hasRoles() {
         return !node.getRoles().isEmpty();
@@ -117,6 +201,13 @@ public class SourceExtractor {
         return (node instanceof StructuralNode) && ((StructuralNode) node).getStyle() != null;
     }
 
+    private boolean hasTitle() {
+        // Section and List titles need to be handled differently
+        // True if node is not a Section or List and has a non-null title
+        return (node instanceof StructuralNode && !((node instanceof Section) || (node instanceof List)))
+                && ((StructuralNode) node).getTitle() != null;
+    }
+
     /**
      * Pulls the id, attributes, style, and roles from the node into the proper syntax
      */
@@ -126,9 +217,23 @@ public class SourceExtractor {
         var hasAttributes = hasAttributes();
         var hasRoles = hasRoles();
         var hasStyle = hasStyle();
+        var hasTitle = hasTitle();
 
-        if (hasId || hasRoles || hasAttributes || hasStyle) {
-            source.append("[");
+        // If the node has any of these (id, roles, attributes, style, or title)
+        // We need to handle it so it is above the node in the output
+        if (hasId || hasRoles || hasAttributes || hasStyle || hasTitle) {
+
+            // Titles could be an actual title (.Some title) above the node
+            // or it could also be an attribute (though you rarely see that)
+            if ((hasAttributes && node.getAttributes().containsKey("title")) || hasTitle) {
+                if (hasTitle)
+                    source.append(".").append(((StructuralNode) node).getTitle()).append("\n");
+                else
+                    source.append(".").append(node.getAttribute("title")).append("\n");
+            }
+
+            // Now we're into the id, role, style, attributes which will surrounded by [ and ]
+            var mainJoiner = new StringJoiner(" ", "[", "]");
 
             if (hasId) {
                 // If there isn't an explicit id for a section, it starts with an _
@@ -137,20 +242,38 @@ public class SourceExtractor {
                     id = id.substring(1).replaceAll("_", "-");
                 }
                 var idWithoutContext = id.split("_")[0];
-                source.append("id=\"").append(idWithoutContext).append("_{context}\"");
+                mainJoiner.add("id=\"" + idWithoutContext + "_{context}\"");
             }
 
-            if (hasStyle)
-                source.append(" ").append(((StructuralNode) node).getStyle());
+            // Style, roles, and attributes are separated by a ',' and not a ' '
+            var joiner = new StringJoiner(",", " ", "");
 
-            if (hasAttributes) {
-                // TODO: Attributes
+            if (hasStyle) {
+                joiner.add(((StructuralNode) node).getStyle());
+
+                // You typically see source and language together, so we'll put them together as well.
+                if (node.hasAttribute("language"))
+                    joiner.add(node.getAttribute("language").toString());
             }
 
             if (hasRoles)
-                source.append(" role=\"").append(String.join(",", node.getRoles())).append("\"");
+                joiner.add("role=\"" + String.join(",", node.getRoles()) + "\"");
 
-            source.append("]");
+            if (hasAttributes) {
+                // We have already added some of the attributes above, we don't need to add them twice.
+                var keys = new java.util.HashSet<>(node.getAttributes().keySet());
+                keys.remove("style");
+                keys.remove("title");
+                keys.remove("language");
+
+                keys.stream()
+                        // We don't really need the positional ones as they'll be listed as named (I think)
+                        .filter(s -> s.matches("\\D+.*"))
+                        .forEach(key -> joiner.add(key + "=\"" + node.getAttribute(key).toString() + "\""));
+            }
+
+            // Join everything together
+            source.append(mainJoiner.merge(joiner).toString());
         }
     }
 }
--- src/test/java/com/redhat/documentation/asciidoc/SourceExtractorTest.java
+++ src/test/java/com/redhat/documentation/asciidoc/SourceExtractorTest.java
@@ -1,10 +1,5 @@
 package com.redhat.documentation.asciidoc;
 
-import java.io.File;
-
-import org.asciidoctor.Asciidoctor;
-import org.asciidoctor.OptionsBuilder;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -60,5 +55,212 @@ class SourceExtractorTest extends AsciidocExtractionTest {
         var extractor = new SourceExtractor(blocks.get(0));
 
         assertThat(extractor.getSource()).isEqualTo(adoc);
+    }
+
+    @Test
+    public void testSourceListing() throws Exception {
+        var adoc = """
+                .app.rb
+                [source,ruby,subs="attributes"]
+                ----
+                require 'sinatra'
+
+                get '/hi' do
+                  "Hello World!"
+                end
+                ----""";
+
+        var document = asciidoctor.load(adoc, optionsBuilder.asMap());
+        var blocks = document.getBlocks();
+
+        assertThat(blocks).hasSize(1);
+        var extractor = new SourceExtractor(blocks.get(0));
+
+        assertThat(extractor.getSource()).isEqualTo(adoc);
+    }
+
+    @Test
+    public void testBasicTable() throws Exception {
+        var adoc = """
+                |===
+                | Row one, Cell one | Row one, Cell two
+                | Row two, Cell one | Row two, Cell two
+                |===""";
+
+        var document = asciidoctor.load(adoc, optionsBuilder.asMap());
+        var blocks = document.getBlocks();
+
+        assertThat(blocks).hasSize(1);
+        var extractor = new SourceExtractor(blocks.get(0));
+
+        var expected = """
+                [table,tablepcwidth="100",rowcount="2",colcount="2"]
+                |===
+                | Row one, Cell one | Row one, Cell two
+                | Row two, Cell one | Row two, Cell two
+                |===""";
+
+        assertThat(extractor.getSource()).isEqualTo(expected);
+    }
+
+    // TODO: I need a more advanced table to test
+
+    @Test
+    public void testBasicOrderedList() throws Exception {
+        var adoc = """
+                . Protons
+                . Electrons
+                . Neutrons""";
+
+        var document = asciidoctor.load(adoc, optionsBuilder.asMap());
+        var blocks = document.getBlocks();
+
+        assertThat(blocks).hasSize(1);
+        var extractor = new SourceExtractor(blocks.get(0));
+
+        var expected = """
+                [arabic]
+                . Protons
+                . Electrons
+                . Neutrons""";
+
+        assertThat(extractor.getSource()).isEqualTo(expected);
+    }
+
+    @Test
+    public void testBasicOrderedListWithNumbers() throws Exception {
+        var adoc = """
+                1. Protons
+                2. Electrons
+                3. Neutrons""";
+
+        var document = asciidoctor.load(adoc, optionsBuilder.asMap());
+        var blocks = document.getBlocks();
+
+        assertThat(blocks).hasSize(1);
+        var extractor = new SourceExtractor(blocks.get(0));
+
+        var expected = """
+                [arabic]
+                . Protons
+                . Electrons
+                . Neutrons""";
+
+        assertThat(extractor.getSource()).isEqualTo(expected);
+    }
+
+    @Test
+    public void testAdvancedOrderedList() throws Exception {
+        var adoc = """
+                [%reversed,start=3]
+                .Title
+                . Protons
+                . Electrons
+                . Neutrons""";
+
+        var document = asciidoctor.load(adoc, optionsBuilder.asMap());
+        var blocks = document.getBlocks();
+
+        assertThat(blocks).hasSize(1);
+        var extractor = new SourceExtractor(blocks.get(0));
+
+        var expected = """
+                [arabic,start="3",reversed-option=""]
+                .Title
+                . Protons
+                . Electrons
+                . Neutrons""";
+
+        assertThat(extractor.getSource()).isEqualTo(expected);
+    }
+
+    @Test
+    public void testComplexListContent() throws Exception {
+        var adoc = """
+                * The header in AsciiDoc must start with a document title.
+                +
+                ----
+                = Document Title
+                ----
+                +
+                Keep in mind that the header is optional.
+
+                * Optional Author and Revision information immediately follows the header title.
+                +
+                ----
+                = Document Title
+                Doc Writer <doc.writer@asciidoc.org>
+                v1.0, 2013-01-01
+                ----""";
+
+        var document = asciidoctor.load(adoc, optionsBuilder.asMap());
+        var blocks = document.getBlocks();
+
+        assertThat(blocks).hasSize(1);
+        var extractor = new SourceExtractor(blocks.get(0));
+
+        var expected = """
+                * The header in AsciiDoc must start with a document title.
+                +
+                [listing]
+                ----
+                = Document Title
+                ----
+                +
+                Keep in mind that the header is optional.
+                * Optional Author and Revision information immediately follows the header title.
+                +
+                [listing]
+                ----
+                = Document Title
+                Doc Writer <doc.writer@asciidoc.org>
+                v1.0, 2013-01-01
+                ----""";
+
+        assertThat(extractor.getSource()).isEqualTo(expected);
+    }
+
+    @Test
+    public void testNestedOrderedList() throws Exception {
+        var adoc = """
+                . Protons
+                .. Electrons
+                . Neutrons""";
+
+        var document = asciidoctor.load(adoc, optionsBuilder.asMap());
+        var blocks = document.getBlocks();
+
+        assertThat(blocks).hasSize(1);
+        var extractor = new SourceExtractor(blocks.get(0));
+
+        var expected = """
+                [arabic]
+                . Protons
+                [loweralpha]
+                .. Electrons
+                . Neutrons""";
+
+        assertThat(extractor.getSource()).isEqualTo(expected);
+    }
+
+    @Test
+    public void testBasicUnorderedList() throws Exception {
+        var adoc = """
+                * Protons
+                * Electrons
+                * Neutrons""";
+
+        var document = asciidoctor.load(adoc, optionsBuilder.asMap());
+        var blocks = document.getBlocks();
+
+        assertThat(blocks).hasSize(1);
+        var extractor = new SourceExtractor(blocks.get(0));
+
+        var expected = """
+                * Protons
+                * Electrons
+                * Neutrons""";
+
+        assertThat(extractor.getSource()).isEqualTo(expected);
     }
 }
